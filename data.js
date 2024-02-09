538e1928aff2b3023d1ff1ea3093c6046af991e1
Jemuel <salcedojemuel25@gmail.com>
2024-02-09 09:42:08 +0800

    first-commit

---
+++ data.js
@@ -0,0 +1,383 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-china",
+      "name": "China",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3504023650080974,
+          "pitch": 0.20100926054096213,
+          "rotation": 13.351768777756625,
+          "target": "1-coron"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.09665400936521,
+          "pitch": -0.2352462655244949,
+          "title": "Great Wall Of China",
+          "text": "<span style=\"color: rgb(228, 230, 235); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(48, 48, 48);\">Great Wall of China (China): Spanning over 13,000 miles, the Great Wall of China is an ancient marvel of engineering and fortification. Constructed over several centuries to protect against invasions, it offers breathtaking views of rugged landscapes and historic watchtowers. Visitors can walk along its winding paths, imagining the ancient soldiers who once stood guard along this iconic symbol of China's rich history and enduring strength.</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-coron",
+      "name": "Coron",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9318773317072413,
+          "pitch": 0.7635484158515755,
+          "rotation": 2.356194490192345,
+          "target": "2-italy"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.35543257503626,
+          "pitch": 0.022274802488400525,
+          "title": "Coron Palawan",
+          "text": "<span style=\"color: rgb(228, 230, 235); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(48, 48, 48);\">Coron, Palawan (Philippines): Nestled in the pristine waters of the Philippines, Coron is a breathtaking destination renowned for its crystal-clear turquoise lagoons, towering limestone cliffs, and vibrant marine life. Home to stunning attractions like Kayangan Lake, Barracuda Lake, and Twin Lagoon, this tropical paradise offers unparalleled opportunities for snorkeling, diving, and island hopping adventures. Exploring Coron's enchanting landscapes, visitors are treated to postcard-perfect views of hidden coves, emerald jungles, and dramatic rock formations, making it a true haven for nature lovers and adventure seekers alike.</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-italy",
+      "name": "Italy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5647257261705612,
+          "pitch": 0.35468617922686363,
+          "rotation": 1.5707963267948966,
+          "target": "3-jesus"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7753181561845697,
+          "pitch": -0.3502246505555302,
+          "title": "<span style=\"background-color: rgba(80, 151, 255, 0.18); color: rgb(4, 12, 40); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">The Colosseum</span>",
+          "text": "<span style=\"color: rgb(228, 230, 235); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(48, 48, 48);\">A symbol of ancient Rome's grandeur and power, the Colosseum is one of the most iconic landmarks in Italy. This massive amphitheater once hosted gladiatorial contests, animal hunts, and dramatic performances, captivating audiences with its architectural splendor and grisly spectacles. Today, visitors can wander through its labyrinthine corridors, imagining the roar of the crowds and the clash of swords echoing through its ancient walls. The Colosseum stands as a testament to the enduring legacy of Roman engineering and entertainment.</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-jesus",
+      "name": "Jesus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.221137997506263,
+          "pitch": 0.17322448142640923,
+          "rotation": 3.141592653589793,
+          "target": "4-mexico"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4861671948571544,
+          "pitch": -0.4238338653563787,
+          "title": "Christ The Redeemer",
+          "text": "<span style=\"color: rgb(228, 230, 235); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(48, 48, 48);\">Towering over the vibrant city of Rio de Janeiro, Christ the Redeemer is an awe-inspiring statue that symbolizes faith, peace, and cultural heritage. Standing atop Corcovado Mountain, this colossal monument welcomes visitors with open arms, offering panoramic views of Rio's stunning coastline and lush tropical landscapes below. A marvel of modern engineering and spiritual significance, it serves as a beacon of hope and unity for people from all walks of life.</span>"
+        }
+      ]
+    },
+    {
+      "id": "4-mexico",
+      "name": "Mexico",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.99880948833675,
+          "pitch": 0.16056995904974158,
+          "rotation": 3.9269908169872414,
+          "target": "5-peru"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2711043887194116,
+          "pitch": -0.46603946417779696,
+          "title": "Chichen Itza",
+          "text": "<span style=\"color: rgb(228, 230, 235); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(48, 48, 48);\">As one of the most important archaeological sites in Mexico, Chichen Itza reveals the grandeur of the ancient Maya civilization. Its iconic pyramid, El Castillo, stands as a testament to Maya astronomical knowledge and architectural prowess. Visitors can marvel at the intricate carvings, ball courts, and temples that dot the site, gaining insight into the rich cultural traditions and spiritual beliefs of the Maya people. Chichen Itza's timeless allure continues to fascinate and inspire all who venture into its ancient ruins.</span>"
+        }
+      ]
+    },
+    {
+      "id": "5-peru",
+      "name": "Peru",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5620260858897979,
+          "pitch": 0.6408453877243723,
+          "rotation": 4.71238898038469,
+          "target": "6-petra-jordan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.39222431064312957,
+          "pitch": -0.0708195252146453,
+          "title": "<span style=\"color: rgb(5, 5, 5); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(240, 240, 240);\">Machu Picchu</span>",
+          "text": "<span style=\"color: rgb(5, 5, 5); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(240, 240, 240);\">Nestled high in the Andes Mountains, Machu Picchu is a testament to the architectural brilliance of the ancient Inca civilization. This \"Lost City of the Incas\" captivates visitors with its terraced fields, stone temples, and intricate urban layout, all set against the backdrop of mist-shrouded peaks. Exploring Machu Picchu is a journey into the heart of Inca culture and history, where every stone tells a story of innovation, spirituality, and the enduring bond between humanity and the natural world.</span>"
+        }
+      ]
+    },
+    {
+      "id": "6-petra-jordan",
+      "name": "Petra Jordan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.224315405643809,
+          "pitch": 0.07575519415785692,
+          "rotation": 5.497787143782138,
+          "target": "7-taj-mahal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5681410734145285,
+          "pitch": -0.4140671916401253,
+          "title": "Petra",
+          "text": "<span style=\"color: rgb(5, 5, 5); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(240, 240, 240);\">Carved into rose-red cliffs by the Nabataeans over 2,000 years ago, Petra is a UNESCO World Heritage Site and one of the most iconic archaeological wonders in the world. Its intricate rock-cut architecture, including the famous Treasury and Monastery, mesmerizes visitors with its blend of natural beauty and human ingenuity. Exploring the narrow Siq canyon leading to Petra's hidden treasures is a journey through time, revealing the splendor of a lost civilization nestled within Jordan's desert landscapes.</span>"
+        }
+      ]
+    },
+    {
+      "id": "7-taj-mahal",
+      "name": "Taj Mahal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9933044044870885,
+          "pitch": -0.007383363286793454,
+          "rotation": 6.283185307179586,
+          "target": "0-china"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.803341411894813,
+          "pitch": -0.3325225426898992,
+          "title": "Taj Mahal",
+          "text": "<span style=\"color: rgb(5, 5, 5); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 15px; white-space-collapse: preserve; background-color: rgb(240, 240, 240);\">Regarded as one of the most exquisite architectural masterpieces in the world, the Taj Mahal is a UNESCO World Heritage Site and an enduring symbol of love. Built by Emperor Shah Jahan in memory of his beloved wife, Mumtaz Mahal, this magnificent mausoleum features intricate marble inlays, delicate carvings, and stunning symmetrical gardens. As the sunlight dances on its gleaming white domes and minarets, visitors are spellbound by the Taj Mahal's timeless beauty and romantic allure, making it a must-visit destination for travelers from around the globe.</span><br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
